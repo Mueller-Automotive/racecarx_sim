b48e055360c0a0ec7d2e315194e6bf7f830f21ae
D. Neykov <metheorita@gmail.com>
2023-05-21 01:07:55 +0200

    Add ackermann steering

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,20 +4,25 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 
+ARGUMENTS = [
+    DeclareLaunchArgument('world', default_value='depot',
+                          description='GZ World'),
+    DeclareLaunchArgument('robot_name', default_value='racercarx',
+                          description='Robot name')
+]
 
 
 def generate_launch_description():
-
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     
     # Make sure package name is correct
-    package_name='mueller-auto' # <--- CHANGE ME
+    package_name='mueller_auto' # <--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -25,23 +30,35 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim') #checked
+    gz_sim_launch = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py']) #checked
+
+    # Gazebo
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gz_sim_launch]),
+        launch_arguments=[
+            ('gz_args', [PathJoinSubstitution([get_package_share_directory(package_name), 'worlds', 'depot.sdf'])])
+        ]
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
-
-
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-world', 'racecarx-depot',
+            '-name', 'racecarx',
+            '-file', PathJoinSubstitution(
+                [get_package_share_directory(package_name),
+                'description',
+                'car.sdf'])
+        ],
+        output='screen'
+    )
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
+        #rsp,
+        gz_sim,
+        spawn_robot,
     ])
