c896b74d2ee48887cccfaefc855a89676ae2b3c3
D. Neykov <metheorita@gmail.com>
2023-05-29 15:46:19 +0200

    Working entity insertion plugin

--- plugins/MyPlugin.cc
+++ plugins/MyPlugin.cc
@@ -2,6 +2,11 @@
 #include <gz/sim/Util.hh>
 #include <gz/sim/System.hh>
 #include <gz/plugin/Register.hh>
+#include <gz/math/Vector4.hh>
+#include <gz/msgs/entity_factory.pb.h>
+#include <gz/transport/Node.hh>
+
+#include <plugins/road_gen/Line.hpp>
 
 using namespace gz;
 using namespace sim;
@@ -22,13 +27,41 @@ class MyPlugin
                             EventManager &/*_eventMgr*/) override
     {
         // Read property from SDF
-        auto linkName = _sdf->Get<std::string>("link_name");
+        auto linkName = _sdf->Get<std::string>("plane2");
 
         // Create model object to access convenient functions
         auto model = Model(_entity);
 
         // Get link entity
-        this->linkEntity = model.LinkByName(_ecm, linkName);
+        this->linkEntity = _entity;
+
+        Line line("sdf-build-example", 
+                gz::math::Vector4<float>(0.0, 0.0, 0.1, 1.0), 
+                gz::math::Vector4<float>(0.0, 0.0, 0.0, 1.0), 
+                gz::math::Vector4<float>(0.0, 0.0, 0.0, 1.0));
+
+        bool result;
+        gz::msgs::EntityFactory req;
+        gz::msgs::Boolean res;
+        req.set_sdf(line.getSdf());
+        req.set_allow_renaming(true);
+
+        unsigned int timeout = 5000;
+
+        bool executed = this->node.Request("/world/racecarx-depot/create", req, timeout, res, result);
+        if (executed)
+        {
+            if (result)
+            std::cout << "Entity was created : [" << res.data() << "]" << std::endl;
+            else
+            {
+            std::cout << "Service call failed" << std::endl;
+            return;
+            }
+        }
+        else
+            std::cerr << "Service call timed out" << std::endl;
+
     }
 
     // Implement PostUpdate callback, provided by ISystemPostUpdate
@@ -37,11 +70,12 @@ class MyPlugin
                             const EntityComponentManager &_ecm) override
     {
         // Get link pose and print it
-        std::cout << worldPose(this->linkEntity, _ecm) << std::endl;
+        //std::cout << worldPose(this->linkEntity, _ecm) << std::endl;
     }
 
     // ID of link entity
     private: Entity linkEntity;
+    private: transport::Node node;
 };
 
 // Register plugin
--- plugins/road_gen/Line.hpp
+++ plugins/road_gen/Line.hpp
@@ -1,14 +1,65 @@
 #pragma once
 
+#include <string>
+
 #include <gz/math/Vector4.hh>
 #include <plugins/road_gen/LineType.hpp>
+
+#define FMT_HEADER_ONLY
+#include <include/fmt/format.h>
 
 class Line
 {
 public:
-    gz::math::Vector4 point1;
-    gz::math::Vector4 point2;
+    std::string name;
+    gz::math::Vector4<float> point1;
+    gz::math::Vector4<float> point2;
+    gz::math::Vector4<float> rot;
     LineType lineType;
+
+    Line(std::string _name, gz::math::Vector4<float> _point1, gz::math::Vector4<float> _point2, gz::math::Vector4<float> _rot)
+    {
+        name = _name;
+        point1 = _point1;
+        point2 = _point2;
+        rot = _rot;
+    }
+
+    virtual std::string getSdf()
+    {
+        std::string model_start = fmt::format("<?xml version='1.0'?><sdf version='1.7'><model name='{}'><static>true</static><self_collide>false</self_collide>", name);
+        std::string model_pose = fmt::format("<pose>{} {} {} {} {} {}</pose>", point1[0], point1[1], point1[2], rot[0], rot[1], rot[2]);
+        std::string model_end = "</model></sdf>";
+
+        std::string plane_start = "<link name='plane1'> \
+<visual name='visual'>\
+<geometry>\
+<plane>\
+<normal>0 0 1</normal>\
+<size>0.4 0.4</size>\
+</plane>\
+</geometry>\
+<material>\
+<ambient>0.7 0.7 0.7 1</ambient>\
+<diffuse>0.7 0.7 0.7 1</diffuse>\
+<specular>0 0 0 0</specular>\
+<emissive>0 0 0 1</emissive>\
+</material>\
+</visual>";
+        
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        float r_x = 0;
+        float r_y = 0;
+        float r_z = 0;
+        
+        std::string plane_pose = fmt::format("<pose>{} {} {} {} {} {}</pose>", x, y, z, r_x, r_y, r_z);
+        std::string plane_end = "</link>";
+
+        std::string sdf = model_start + model_pose + plane_start + plane_pose + plane_end + model_end;
+        return sdf;
+    }
 private:
     
 };
