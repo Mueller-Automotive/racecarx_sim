7c4b0a177e54108b9a0b7a224c7f7ba94f38fa7c
D. Neykov <metheorita@gmail.com>
2023-06-14 15:59:20 +0200

    Add camera bridge

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
         ]
     )
 
-    # Bridge Twist messages between ROS and Gazebo
+    # Bridge Twist messages between ROS and Gazebo - Gazebo <- ROS
     twist_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
                             name='twist_bridge',
                             output='screen',
@@ -39,7 +39,7 @@ def generate_launch_description():
                                 '/cmd_vel' + '@geometry_msgs/msg/Twist' + ']gz.msgs.Twist',
                             ])
 
-    # Bridge Actuators messages between ROS and Gazebo
+    # Actuators bridge - Gazebo <- ROS
     actuators_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
                             name='actuators_bridge',
                             output='screen',
@@ -49,7 +49,30 @@ def generate_launch_description():
                             arguments=[
                                 '/actuators' + '@actuator_msgs/msg/Actuators' + ']gz.msgs.Actuators',
                             ])
-    
+
+
+    # Lidar bridge - Gazebo -> ROS
+    lidar_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                            name='lidar_bridge',
+                            output='screen',
+                            parameters=[{
+                                'use_sim_time': True
+                            }],
+                            arguments=[
+                                '/lidar' + '@sensor_msgs/msg/LaserScan' + '[gz.msgs.LaserScan',
+                            ])
+
+    # Camera bridge - Gazebo -> ROS
+    camera_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+                            name='camera_bridge',
+                            output='screen',
+                            parameters=[{
+                                'use_sim_time': True
+                            }],
+                            arguments=[
+                                '/camera' + '@sensor_msgs/msg/Image' + '[gz.msgs.Image',
+                            ])
+                            
     # Spawn an entity of the car in the world we started
     spawn_robot = Node(
         package='ros_gz_sim',
@@ -72,6 +95,8 @@ def generate_launch_description():
     return LaunchDescription([
         twist_bridge,
         actuators_bridge,
+        camera_bridge,
+        lidar_bridge,
         gz_sim,
         spawn_robot,
     ])
